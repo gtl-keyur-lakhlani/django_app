a184c4282c8a64c6198b3098aae6f2af17679666
Keyur Lakhlani <keyur.lakhlani@gatewaygroup.com>
2019-07-31 10:50:11 +0530

    Revert "API - Create Questions and Admin Choice Module Enable"
    This reverts commit d6242cb709702ce7781617310bf73c9cfdaf5f87.

--- polls/admin.py
+++ polls/admin.py
@@ -2,8 +2,5 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Question
-from .models import Choice
-
 
 admin.site.register(Question)
-admin.site.register(Choice)
--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
 from . import views
-from . import apiviews
 
 # app_name = 'polls'
 # urlpatterns = [
@@ -16,5 +15,4 @@ urlpatterns = [
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
-    path('questions/', apiviews.questions_view, name='questions_view'),
 ]
