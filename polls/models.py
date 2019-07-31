8e9256e080f3830d3f77b952f19cbb4a5c808be8
Keyur Lakhlani <keyur.lakhlani@gatewaygroup.com>
2019-07-31 18:49:09 +0530

    API - Question list with derived field

--- polls/models.py
+++ polls/models.py
@@ -8,12 +8,15 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-
     def __str__(self):
         return self.question_text
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def verbose_question_text(self):
+        return "Question : %s" % (self.question_text)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
