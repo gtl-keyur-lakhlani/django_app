0cabc7f6b75700175c19ad6939633db21e0cff2b
Keyur Lakhlani <keyur.lakhlani@gatewaygroup.com>
2019-08-01 13:54:31 +0530

    API - Question List with Choices

--- polls/models.py
+++ polls/models.py
@@ -17,6 +17,11 @@ class Question(models.Model):
     def verbose_question_text(self):
         return "Question : %s" % (self.question_text)
 
+    def choices(self):
+        if not hasattr(self, '_choices'):
+            self._choices = self.choice_set.all()
+        return self._choices
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
